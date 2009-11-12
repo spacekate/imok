b48262d4a5a212b6be4717b81a939b52c2e3c75e
Kate Currie <katecurrie@kismet.local>
2009-11-12 10:14:21 +1000

    fixed failing unit test - removed the username

--- test/CustomerLogicTests.py
+++ test/CustomerLogicTests.py
@@ -5,9 +5,9 @@ from CustomerLogic import *
 
 class CustomerLogicTest(unittest.TestCase):     
     def testCreate(self):
-        createAccount('u', 'p', 'n', 'e@currie.to', 'ph', 'mob')
+        createAccount('e@currie.to', 'p', 'n', 'ph', 'mob')
         try:
-            createAccount('u', 'p', 'n', 'e@currie.to', 'ph', 'mob')
+            createAccount('e@currie.to', 'p', 'n', 'ph', 'mob')
             self.fail('Should not have been allowed to create 2 accounts with same username')
         except AccountExistsException, e:
             pass
